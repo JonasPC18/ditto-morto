d19fbbce1d37caa10f06d3b33d74f9ba50f3018d
JonasPC18 <134112450+JonasPC18@users.noreply.github.com>
2025-07-21 08:08:15 -0300

    animação home; responsividade no carousel; add quadrinhos

--- src/data/comics.js
+++ src/data/comics.js
@@ -1,3 +1,4 @@
+//Quadrinho 1
 import pg1a from '../assets/quadrinho1/1.jpg';
 import pg1b from '../assets/quadrinho1/2.jpg';
 import pg1c from '../assets/quadrinho1/3.jpg';
@@ -8,7 +9,7 @@ import pg1g from '../assets/quadrinho1/7.jpg';
 import pg1h from '../assets/quadrinho1/8.jpg';
 import pg1i from '../assets/quadrinho1/9.jpg';
 
-
+//Quadrinho 2
 import pg2a from '../assets/quadrinho2/1.jpg';
 import pg2b from '../assets/quadrinho2/2.jpg';
 import pg2c from '../assets/quadrinho2/3.jpg';
@@ -19,6 +20,125 @@ import pg2g from '../assets/quadrinho2/7.jpg';
 import pg2h from '../assets/quadrinho2/8.jpg';
 import pg2i from '../assets/quadrinho2/9.jpg';
 import pg2j from '../assets/quadrinho2/10.jpg';
+
+//Quadrinho 3
+import pg3a from '../assets/quadrinho3/1.jpg';
+import pg3b from '../assets/quadrinho3/2.jpg';
+import pg3c from '../assets/quadrinho3/3.jpg';
+import pg3d from '../assets/quadrinho3/4.jpg';
+import pg3e from '../assets/quadrinho3/5.mp4';
+import pg3f from '../assets/quadrinho3/6.jpg';
+import pg3g from '../assets/quadrinho3/7.jpg';
+import pg3h from '../assets/quadrinho3/8.jpg';
+import pg3i from '../assets/quadrinho3/9.jpg';
+import pg3j from '../assets/quadrinho3/10.jpg';
+
+//Quadrinho 4
+import pg4a from '../assets/quadrinho4/1.jpg';
+import pg4b from '../assets/quadrinho4/2.jpg';
+import pg4c from '../assets/quadrinho4/3.jpg';
+import pg4d from '../assets/quadrinho4/4.jpg';
+import pg4e from '../assets/quadrinho4/5.jpg';
+import pg4f from '../assets/quadrinho4/6.jpg';
+import pg4g from '../assets/quadrinho4/7.jpg';
+import pg4h from '../assets/quadrinho4/8.jpg';
+import pg4i from '../assets/quadrinho4/9.jpg';
+import pg4j from '../assets/quadrinho4/10.jpg';
+
+//Quadrinho 5
+import pg5a from '../assets/quadrinho5/1.jpg';
+import pg5b from '../assets/quadrinho5/2.jpg';
+import pg5c from '../assets/quadrinho5/3.jpg';
+import pg5d from '../assets/quadrinho5/4.jpg';
+import pg5e from '../assets/quadrinho5/5.jpg';
+import pg5f from '../assets/quadrinho5/6.jpg';
+import pg5g from '../assets/quadrinho5/7.jpg';
+import pg5h from '../assets/quadrinho5/8.jpg';
+import pg5i from '../assets/quadrinho5/9.jpg';
+
+//Quadrinho 6
+import pg6a from '../assets/quadrinho6/1.jpg';
+import pg6b from '../assets/quadrinho6/2.jpg';
+import pg6c from '../assets/quadrinho6/3.jpg';
+import pg6d from '../assets/quadrinho6/4.jpg';
+import pg6e from '../assets/quadrinho6/5.jpg';
+import pg6f from '../assets/quadrinho6/6.mp4';
+import pg6g from '../assets/quadrinho6/7.mp4';
+import pg6h from '../assets/quadrinho6/8.mp4';
+import pg6i from '../assets/quadrinho6/9.mp4';
+import pg6j from '../assets/quadrinho6/10.mp4';
+
+//Quadrinho 7
+import pg7a from '../assets/quadrinho7/1.jpg';
+import pg7b from '../assets/quadrinho7/2.mp4';
+import pg7c from '../assets/quadrinho7/3.mp4';
+import pg7d from '../assets/quadrinho7/4.mp4';
+import pg7e from '../assets/quadrinho7/5.mp4';
+import pg7f from '../assets/quadrinho7/6.mp4';
+import pg7g from '../assets/quadrinho7/7.mp4';
+import pg7h from '../assets/quadrinho7/8.mp4';
+import pg7i from '../assets/quadrinho7/9.mp4';
+import pg7j from '../assets/quadrinho7/10.jpg';
+
+// Quadrinho 8
+import pg8a from '../assets/quadrinho8/1.jpg';
+import pg8b from '../assets/quadrinho8/2.jpg';
+import pg8c from '../assets/quadrinho8/3.jpg';
+import pg8d from '../assets/quadrinho8/4.jpg';
+import pg8e from '../assets/quadrinho8/5.jpg';
+import pg8f from '../assets/quadrinho8/6.jpg';
+import pg8g from '../assets/quadrinho8/7.jpg';
+import pg8h from '../assets/quadrinho8/8.jpg';
+import pg8i from '../assets/quadrinho8/9.jpg';
+import pg8j from '../assets/quadrinho8/10.jpg';
+
+// Quadrinho 9
+import pg9a from '../assets/quadrinho9/1.jpg';
+import pg9b from '../assets/quadrinho9/2.jpg';
+import pg9c from '../assets/quadrinho9/3.jpg';
+import pg9d from '../assets/quadrinho9/4.jpg';
+import pg9e from '../assets/quadrinho9/5.jpg';
+import pg9f from '../assets/quadrinho9/6.jpg';
+import pg9g from '../assets/quadrinho9/7.jpg';
+import pg9h from '../assets/quadrinho9/8.jpg';
+import pg9i from '../assets/quadrinho9/9.jpg';
+import pg9j from '../assets/quadrinho9/10.mp4';
+
+// Quadrinho 10
+import pg10a from '../assets/quadrinho10/1.jpg';
+import pg10b from '../assets/quadrinho10/2.jpg';
+import pg10c from '../assets/quadrinho10/3.jpg';
+import pg10d from '../assets/quadrinho10/4.jpg';
+import pg10e from '../assets/quadrinho10/5.jpg';
+import pg10f from '../assets/quadrinho10/6.jpg';
+import pg10g from '../assets/quadrinho10/7.jpg';
+import pg10h from '../assets/quadrinho10/8.jpg';
+import pg10i from '../assets/quadrinho10/9.jpg';
+import pg10j from '../assets/quadrinho10/10.jpg';
+
+// Quadrinho 11
+import pg11a from '../assets/quadrinho11/1.jpg';
+import pg11b from '../assets/quadrinho11/2.jpg';
+import pg11c from '../assets/quadrinho11/3.jpg';
+import pg11d from '../assets/quadrinho11/4.jpg';
+import pg11e from '../assets/quadrinho11/5.jpg';
+import pg11f from '../assets/quadrinho11/6.jpg';
+import pg11g from '../assets/quadrinho11/7.jpg';
+import pg11h from '../assets/quadrinho11/8.jpg';
+import pg11i from '../assets/quadrinho11/9.jpg';
+import pg11j from '../assets/quadrinho11/10.jpg';
+
+// Quadrinho 12
+import pg12a from '../assets/quadrinho12/1.jpg';
+import pg12b from '../assets/quadrinho12/2.jpg';
+import pg12c from '../assets/quadrinho12/3.jpg';
+import pg12d from '../assets/quadrinho12/4.jpg';
+import pg12e from '../assets/quadrinho12/5.jpg';
+import pg12f from '../assets/quadrinho12/6.jpg';
+import pg12g from '../assets/quadrinho12/7.jpg';
+import pg12h from '../assets/quadrinho12/8.jpg';
+import pg12i from '../assets/quadrinho12/9.jpg';
+import pg12j from '../assets/quadrinho12/10.jpg';
 
 export const comics = [
   {
@@ -32,5 +152,70 @@ export const comics = [
     title: "Cicatriz",
     cover: pg2a,
     pages: [pg2a,pg2b,pg2c,pg2d,pg2e,pg2f,pg2g,pg2h,pg2i,pg2j]
+  },
+
+{
+    id: "ditto03",
+    title: "Espelho",
+    cover: pg3a,
+    pages: [pg3a,pg3b,pg3c,pg3d,pg3e,pg3f,pg3g,pg3h,pg3i,pg3j]
+  },
+
+  {
+    id: "ditto04",
+    title: "Amigo Secreto",
+    cover: pg4a,
+    pages: [pg4a,pg4b,pg4c,pg4d,pg4e,pg4f,pg4g,pg4h,pg4i,pg4j]
+  },
+
+    {
+    id: "ditto05",
+    title: "A Namorada",
+    cover: pg5a,
+    pages: [pg5a,pg5b,pg5c,pg5d,pg5e,pg5f,pg5g,pg5h,pg5i]
+  },
+
+      {
+    id: "ditto06",
+    title: "Fogo",
+    cover: pg6a,
+    pages: [pg6a,pg6b,pg6c,pg6d,pg6e,pg6f,pg6g,pg6h,pg6i,pg6j]
+  },
+  {
+    id: 'ditto07',
+    title: 'Juju e Jojo',
+    cover: pg7a,
+    pages: [pg7a, pg7b, pg7c, pg7d, pg7e, pg7f, pg7g, pg7h, pg7i, pg7j],
+  },
+  {
+    id: 'ditto08',
+    title: 'Palavras Mortas',
+    cover: pg8a,
+    pages: [pg8a, pg8b, pg8c, pg8d, pg8e, pg8f, pg8g, pg8h, pg8i, pg8j],
+  },
+  {
+    id: 'ditto09',
+    title: 'Cabeça Cheia',
+    cover: pg9a,
+    pages: [pg9a, pg9b, pg9c, pg9d, pg9e, pg9f, pg9g, pg9h, pg9i, pg9j],
+  },
+  {
+    id: 'ditto10',
+    title: 'Memória',
+    cover: pg10a,
+    pages: [pg10a, pg10b, pg10c, pg10d, pg10e, pg10f, pg10g, pg10h, pg10i, pg10j],
+  },
+  {
+    id: 'ditto11',
+    title: 'Rebecca',
+    cover: pg11a,
+    pages: [pg11a, pg11b, pg11c, pg11d, pg11e, pg11f, pg11g, pg11h, pg11i, pg11j],
+  },
+  {
+    id: 'ditto12',
+    title: 'O Espantalho',
+    cover: pg12a,
+    pages: [pg12a, pg12b, pg12c, pg12d, pg12e, pg12f, pg12g, pg12h, pg12i, pg12j],
   }
+
 ];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,34 @@
-// Home.jsx
+import { useEffect, useState } from 'react';
+
+import capa_dm01 from '../assets/capa_dm01.jpg';
 import capa_dm02 from '../assets/capa_dm02.jpg';
+import capa_dm03 from '../assets/capa_dm03.jpg';
+
+const frames = [capa_dm01, capa_dm02, capa_dm03];
 
 export default function Home() {
+  const [idx, setIdx] = useState(0);
+
+  // troca a cada 2 frames de 24 fps  →  1000/24*2 ≈ 83 ms
+  useEffect(() => {
+    // pré‑carrega para evitar flicker
+    frames.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+    });
+
+    const interval = setInterval(
+      () => setIdx((i) => (i + 1) % frames.length),
+      1000 / 24 * 2
+    );
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <img className="imagem_capa" src={capa_dm02} alt="Capa" />
+    <img
+      className="imagem_capa"
+      src={frames[idx]}
+      alt={`Capa ${idx + 1}`}
+    />
   );
 }
